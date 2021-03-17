11f09bc6b9faa2e453fa8e6f5b05e9ae7cb0d0a4
opikoenkz <opikoenkz@gmail.com>
2021-03-17 11:06:14 +0700

    initial commit

---
+++ asset/main.js
@@ -0,0 +1,33 @@
+/*----- Navbar Hamburger -----*/
+const menuBtn = document.querySelector(".menu-btn");
+const menuItems = document.querySelector(".navbar-menu");
+const menuItem = document.querySelectorAll(".nav-link");
+
+// main toggle
+menuBtn.addEventListener("click", () => {
+    toggle();
+});
+
+// toggle on item click if open
+menuItem.forEach((item) => {
+    item.addEventListener("click", () => {
+        if (menuBtn.classList.contains("open")){
+            toggle();
+        }
+    });
+});
+
+function toggle() {
+    menuBtn.classList.toggle("open");
+    menuItems.classList.toggle("open");
+}
+
+
+/*----- Modal-Content -----*/
+function onClick(element) {
+    document.getElementById("img01").src = element.src;
+    document.getElementById("modal01").style.display = "block";
+}
+
+
+
