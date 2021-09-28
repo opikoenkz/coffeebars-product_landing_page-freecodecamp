83440e93c726ab9d8254ea7b7719182eb26a0741
opikoenkz <opikoenkz@gmail.com>
2021-09-28 11:04:24 +0700

    update kedua

--- asset/main.js
+++ asset/main.js
@@ -5,27 +5,27 @@ const menuItem = document.querySelectorAll(".nav-link");
 
 // main toggle
 menuBtn.addEventListener("click", () => {
-    toggle();
+  toggle();
 });
 
 // toggle on item click if open
 menuItem.forEach((item) => {
-    item.addEventListener("click", () => {
-        if (menuBtn.classList.contains("open")){
-            toggle();
-        }
-    });
+  item.addEventListener("click", () => {
+    if (menuBtn.classList.contains("open")) {
+      toggle();
+    }
+  });
 });
 
 function toggle() {
-    menuBtn.classList.toggle("open");
-    menuItems.classList.toggle("open");
+  menuBtn.classList.toggle("open");
+  menuItems.classList.toggle("open");
 }
 
 /*----- Modal-Content -----*/
 function onClick(element) {
-    document.getElementById("img01").src = element.src;
-    document.getElementById("modal01").style.display = "block";
-    let captionText = document.getElementById("caption");
-    captionText.innerHTML = element.alt; 
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+  let captionText = document.getElementById("caption");
+  captionText.innerHTML = element.alt;
 }
